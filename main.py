20a43378d68a7aebe7c481b73620ca3d6ca9f3be
Andrew Carter <68480855+AndrewCarter04@users.noreply.github.com>
2023-02-23 19:42:39 +0000

    Add triangle and pentagon methods

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ import turtleshortcut
 t = turtle.Turtle()
 ts = turtleshortcut.TurtleShortcut(t)
 
-ts.square(10)
-ts.square(20)
-ts.square(30)
+t.pencolor('blue')
+
+ts.square(50)
+ts.square(75)
+ts.square(100)
+
+t.pencolor('orange')
+
+ts.triangle(50)
+ts.triangle(75)
+ts.triangle(100)
+
+t.pencolor('green')
+
+ts.pentagon(50)
+ts.pentagon(75)
+ts.pentagon(100)
--- turtleshortcut.py
+++ turtleshortcut.py
@@ -10,3 +10,13 @@ class TurtleShortcut:
             self.t.forward(length)
             self.t.right(90)
     
+    def triangle(self, length):
+        for i in range(0,3):
+            self.t.forward(length)
+            self.t.right(120)
+
+    def pentagon(self, length):
+        for i in range(0,5):
+            self.t.forward(length)
+            self.t.right(360/5)
+        
